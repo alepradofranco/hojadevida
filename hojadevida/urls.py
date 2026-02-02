0a553905b05df3b1e1007dd4d7d5966c31cebb58
alepradofranco <alepradofranco28@gmail.com>
2026-02-01 20:39:36 -0500

    Corrigiendo urls para servir media en Render

--- hojadevida/urls.py
+++ hojadevida/urls.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path # He añadido re_path aquí
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve # Lo movemos arriba por orden
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('tasks.urls')),  # conecta todas las rutas de tu app "tasks"
+    path('', include('tasks.urls')), 
 ]
 
-# Para servir archivos multimedia (ej. fotos, certificados) en Render
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Configuración universal para archivos Media
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # Esta es la parte mágica para Render
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
